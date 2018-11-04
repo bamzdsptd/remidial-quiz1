c651df7b09f7e9ea32a9cca45d6024bbb5596e54
bamzdsptd <bambangdnr09@gmail.com>
2018-11-04 23:51:04 +0700

    membuat kelas cpu

---
+++ src/REMIDI/cpu.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package REMIDI;
+
+/**
+ *
+ * @author Bamz Dnr
+ */
+public class cpu {
+    private String nama;
+    
+    public cpu (String nama){
+        this.nama = nama;
+    }
+    
+    public String getNama(){
+        return this.nama;
+    }
+}
