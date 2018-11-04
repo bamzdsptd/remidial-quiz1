43b96996ded4149e52bd83c32628843a98318105
bamzdsptd <bambangdnr09@gmail.com>
2018-11-04 23:55:40 +0700

    membuat kelas mouse

---
+++ src/REMIDI/mouse.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package REMIDI;
+
+/**
+ *
+ * @author Bamz Dnr
+ */
+public class mouse {
+    private String merk;
+    
+    public mouse (String merk){
+        this.merk = merk;
+    }
+    
+    public String getMerk(){
+        return this.merk;
+    }
+}
