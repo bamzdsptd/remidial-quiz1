35421468b39fadac432ddb1da8bfef68f1edc330
bamzdsptd <bambangdnr09@gmail.com>
2018-11-04 23:53:31 +0700

    membuat kelas keyboard

---
+++ src/REMIDI/keyboard.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package REMIDI;
+
+/**
+ *
+ * @author Bamz Dnr
+ */
+public class keyboard {
+    private String jenis;
+    
+    public keyboard (String jenis){
+        this.jenis = jenis;
+    }
+    
+    public String getJenis(){
+        return this.jenis;
+    }
+}
